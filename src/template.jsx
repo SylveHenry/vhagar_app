7c39a07eeaccfd0d54585d7956b9ef63f480ffde
Sylvester Henry <henrysylvester93@gmail.com>
2024-07-31 08:46:58 +0000

    modifications ongoing

--- src/template.jsx
+++ src/template.jsx
@@ -12,23 +12,23 @@ import { useWallet } from "@solana/wallet-adapter-react";
 const navitems = [
   {
     text: "about",
-    link: "https://winterstorm.finance/#about",
+    link: "https://vhager.finance/#about",
   },
   {
     text: "tokenomics",
-    link: "https://winterstorm.finance/#tokenomics",
+    link: "https://vhager.finance/#tokenomics",
   },
   {
     text: "roadmap",
-    link: "https://winterstorm.finance/#roadmap",
+    link: "https://vhager.finance/#roadmap-container",
   },
   {
     text: "community",
-    link: "https://winterstorm.finance/#community",
+    link: "https://vhager.finance/#network",
   },
   {
-    text: "whitepaper",
-    link: "https://docs.winterstorm.finance",
+    text: "greenpaper",
+    link: "https://docs.vhager.finance",
   },
 ];
 
@@ -79,7 +79,7 @@ const InfyNft = () => {
             }`}
           >
             <div className="md:max-w-[1120px] flex items-center justify-between  md:px-0 md:gap-5 mb-8 md:mb-16 container  md:mx-auto">
-              <a href="#" className="z-10">
+              <a href="https://vhager.finance/" className="z-10">
                 <img className="w-28 " src={svg_logo} alt="logo" />
               </a>
               <div className="gap-5 md:flex hidden z-10">
@@ -152,10 +152,10 @@ const InfyNft = () => {
           <div className="px-5 xl:px-0 z-10 relative">
             <div className="page-item">
               <h3 className="uppercase text-center text-gradient mb-3 text-3xl font-semibold font-cartoon">
-                VHAGAR staking pool
+                VHAGER staking pool
               </h3>
               <p className="text-center text-lg mb-3">
-                Vhagar on Solana Staking Pool.
+                Vhager on Solana Staking Pool.
               </p>
 
               {/* stake container */}
